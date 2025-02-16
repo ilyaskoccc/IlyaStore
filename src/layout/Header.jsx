00ac14398c7e59c23bbce135895ec68a2b4fa9d4
ilyaskoccc <ilyas_1997koc@hotmail.com>
2025-02-16 03:04:06 +0300

    I optimized the desktop and mobile layout by making the header component responsive. I improved the mobile navigation with a menu transition and added animation.

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,40 +1,29 @@
-import { Heart, Search, ShoppingCart, User } from "lucide-react";
+import { Heart, Menu, Search, ShoppingCart, User, X } from "lucide-react";
+import { useState } from "react";
+
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <header className="h-24 justify-items-center">
-      <nav className="flex justify-evenly items-center w-[1322px] h-full">
+    <header className="container mx-auto">
+      <nav className="px-4 py-8 md:px-0 flex justify-between md:justify-around items-center">
         <div>
           <Link to="/">
             <h3 className="font-bold text-2xl">IlyaStore</h3>
           </Link>
         </div>
-        <div className="w-[815px] flex-row flex justify-between items-center">
-          <div className="space-x-4">
-            <Link to="/" className="font-bold text-[14px] text-[#737373]">
-              Home
-            </Link>
-            <Link to="/shop" className="font-bold text-[14px] text-[#737373]">
-              Shop
-            </Link>
-            <Link to="/about" className="font-bold text-[14px] text-[#737373]">
-              About
-            </Link>
-            <Link to="/blog" className="font-bold text-[14px] text-[#737373]">
-              Blog
-            </Link>
-            <Link
-              to="/contact"
-              className="font-bold text-[14px] text-[#737373]"
-            >
-              Contact
-            </Link>
-            <Link to="/pages" className="font-bold text-[14px] text-[#737373]">
-              Pages
-            </Link>
+        <div className="flex-row hidden md:flex justify-between items-center gap-40">
+          <div className="font-bold text-[14px] text-[#737373] space-x-4">
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+            <Link to="/about">About</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/pages">Pages</Link>
           </div>
-          <div className="flex justify-around items-center space-x-4">
+          <div className="flex items-center space-x-4">
             <User className="text-[#23A6F0] m-0 w-4 h-4" />
             <Link
               to="/login"
@@ -53,18 +42,59 @@ export default function Header() {
               <Search className="text-[#23A6F0] w-4 h-4" />
             </Link>
             <Link to="/shopping-cart">
-              <span className="text-[#23A6F0] flex justify-center items-center space-x-1">
+              <span className="text-[#23A6F0] flex">
                 <ShoppingCart className="w-4 h-4 m-1" /> 1
               </span>
             </Link>
             <Link to="/favorites">
-              <span className="text-[#23A6F0] flex justify-center items-center space-x-1">
+              <span className="text-[#23A6F0] flex">
                 <Heart className="w-4 h-4 m-1" /> 1
               </span>
             </Link>
           </div>
         </div>
+        <div className="flex gap-12 md:hidden">
+          <User className="transition-transform duration-300 hover:scale-110" />
+          <Search className="transition-transform duration-300 hover:scale-110" />
+          <ShoppingCart className="transition-transform duration-300 hover:scale-110" />
+
+          <button
+            className="hover:text-gray-400 focus:outline-none transition-all duration-300"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <span
+              className={`block transition-transform duration-300 ${
+                isMenuOpen ? "rotate-180" : "rotate-0"
+              }`}
+            >
+              {isMenuOpen ? <X /> : <Menu />}
+            </span>
+          </button>
+        </div>
       </nav>
+      {isMenuOpen && (
+        <div
+          className={`text-3xl font-normal text-[#737373] flex flex-col gap-6 pb-10 md:hidden
+          transition-all duration-300 ease-in-out transform origin-top ${
+            isMenuOpen
+              ? "opacity-100 scale-100"
+              : "opacity-0 scale-95 -translate-y-5 pointer-events-none"
+          }`}
+        >
+          <span>
+            <Link to="/">Home</Link>
+          </span>
+          <span>
+            <Link to="/product">Product</Link>
+          </span>
+          <span>
+            <Link to="/pricing">Pricing</Link>
+          </span>
+          <span>
+            <Link to="/contact">Contact</Link>
+          </span>
+        </div>
+      )}
     </header>
   );
 }
