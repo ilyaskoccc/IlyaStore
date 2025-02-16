df2ffc9b09e0a2337d455e954172fc77f0092d53
ilyaskoccc <ilyas_1997koc@hotmail.com>
2025-02-16 03:02:16 +0300

    For the footer component, I optimized the desktop and mobile layout, updated the styles and added the YouTube icon to the mobile version.

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,81 +3,80 @@ import {
   faFacebook,
   faInstagram,
   faTwitter,
+  faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
 import { Mail, MapPin, Phone } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="h-[488px]">
-      <div className="flex-col justify-items-center bg-[#252B42] text-white text-left">
-        <div className="flex justify-between items-center h-36 w-[1050px]">
-          <div className="flex flex-col">
-            <h3 className="font-bold text-[24px]">
+    <footer className="container mx-auto bg-[#252B42] text-white">
+      <div className="px-29 py-10 md:px-40 text-left">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center font-bold">
+          <div className="flex flex-col gap-3">
+            <h3 className="text-[24px]/8">
               Consulting Agency For Your Business
             </h3>
-            <p className="font-normal text-[14px]">
+            <p className="font-normal text-[14px]/5 w-45 md:w-full">
               the quick fox jumps over the lazy dog
             </p>
           </div>
-          <div className="flex justify-center w-[236px] h-[52px]">
-            <span className="w-40 h-[52px] rounded-xl text-center flex justify-center items-center bg-[#23A6F0]">
-              Button
+          <div className="flex py-10">
+            <span className="text-[14px]/[22px] w-40 h-[52px] rounded-xl text-center flex justify-center items-center bg-[#23A6F0]">
+              Contact Us
             </span>
           </div>
         </div>
-        <div className="flex justify-between items-center h-72 w-[1050px]">
-          <div className="flex flex-col">
-            <h5 className="font-bold text-[16px]">Company Info</h5>
-            <div className="flex flex-col gap-2 pt-2">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center font-bold">
+          <div className="flex flex-col gap-3 py-5">
+            <h5 className="text-[16px]">Company Info</h5>
+            <div className="flex flex-col gap-4 pt-3 text-[14px]">
               <Link to="/">About Us</Link>
               <Link to="/">Carrier</Link>
               <Link to="/">We are hiring</Link>
               <Link to="/">Blog</Link>
             </div>
           </div>
-          <div className="flex flex-col">
-            <h5 className="font-bold text-[16px]">Legal</h5>
-            <div className="flex flex-col gap-2 pt-2">
+          <div className="flex flex-col gap-3 py-5">
+            <h5 className="text-[16px]">Legal</h5>
+            <div className="flex flex-col gap-4 pt-3 text-[14px]">
               <Link to="/">About Us</Link>
               <Link to="/">Carrier</Link>
               <Link to="/">We are hiring</Link>
               <Link to="/">Blog</Link>
             </div>
           </div>
-          <div className="flex flex-col">
-            <h5 className="font-bold text-[16px]">Features</h5>
-            <div className="flex flex-col gap-2 pt-2">
+          <div className="flex flex-col gap-3 py-5">
+            <h5 className="text-[16px]">Features</h5>
+            <div className="flex flex-col gap-4 pt-3 text-[14px]">
               <Link to="/">Business Marketing</Link>
               <Link to="/">User Analytic</Link>
               <Link to="/">Live Chat</Link>
               <Link to="/">Unlimited Support</Link>
             </div>
           </div>
-          <div className="flex flex-col">
-            <h5 className="font-bold text-[16px]">Resources</h5>
-            <div className="flex flex-col gap-2 pt-2">
+          <div className="flex flex-col gap-3 py-5">
+            <h5 className="text-[16px]">Resources</h5>
+            <div className="flex flex-col gap-4 pt-3 text-[14px]">
               <Link to="/">IOS & Android</Link>
               <Link to="/">Watch a Demo</Link>
               <Link to="/">Customers</Link>
               <Link to="/">API</Link>
             </div>
           </div>
-          <div className="flex flex-col w-[321px]">
-            <h5 className="font-bold text-[16px]">Get In Touch</h5>
-            <div className="flex flex-col gap-4 pt-2">
-              <div className="flex gap-2">
-                <Phone />
+          <div className="flex flex-col gap-3 py-5">
+            <h5 className="text-[16px]">Get In Touch</h5>
+            <div className="flex flex-col gap-4 pt-3 text-[14px]">
+              <div className="flex items-center gap-2">
+                <Phone className="text-[#8EC2F2]" />
                 <h6 className="font-bold text-[14px]">(480) 555-0103</h6>
               </div>
-              <div className="flex gap-2">
-                <MapPin />
-                <h6 className="font-bold text-[14px] w-[239px]">
-                  4517 Washington Ave.
-                </h6>
+              <div className="flex items-center gap-2">
+                <MapPin className="text-[#8EC2F2]" />
+                <h6 className="font-bold text-[14px] ">4517 Washington Ave.</h6>
               </div>
-              <div className="flex gap-2">
-                <Mail />
+              <div className="flex items-center gap-2">
+                <Mail className="text-[#8EC2F2]" />
                 <h6 className="font-bold text-[14px]">
                   debra.holt@example.com
                 </h6>
@@ -85,23 +84,23 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className="flex justify-between items-center h-20 w-[1050px]">
-          <div className="font-bold text-[14px]">
-            Made With Love By Finland All Right Reserved
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center pt-15">
+          <div className="font-bold text-[14px]/6 w-50 md:w-full">
+            Made With Love By Turkey All Right Reserved
           </div>
-          <div className="flex justify-center gap-4 w-[236px] h-6">
-            <FontAwesomeIcon
-              icon={faFacebook}
-              className="text-[#23A6F0] text-[23px]"
-            />
-            <FontAwesomeIcon
-              icon={faInstagram}
-              className="text-[#23A6F0] text-[23px]"
-            />
-            <FontAwesomeIcon
-              icon={faTwitter}
-              className="text-[#23A6F0] text-[23px]"
-            />
+          <div className="flex pt-10 gap-4 text-[24px]">
+            <span className="text-[#335BF5] md:text-[#23A6F0]">
+              <FontAwesomeIcon icon={faFacebook} />
+            </span>
+            <span className="text-[#E51F5A] md:text-[#23A6F0]">
+              <FontAwesomeIcon icon={faInstagram} />
+            </span>
+            <span className="text-[#21A6DF] md:text-[#23A6F0]">
+              <FontAwesomeIcon icon={faTwitter} />
+            </span>
+            <span className="text-[#E42F08] flex md:hidden items-center">
+              <FontAwesomeIcon icon={faYoutube} />
+            </span>
           </div>
         </div>
       </div>
