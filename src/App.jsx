3f96f4835b17146d9ae490a7f350ca5f455d7003
ilyaskoccc <ilyas_1997koc@hotmail.com>
2025-02-16 03:00:15 +0300

    I added relative positioning to the root div in App.jsx for layout adjustments.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
+    <div className="relative">
       <Switch>
         <Route exact path="/">
           <HomePage />
